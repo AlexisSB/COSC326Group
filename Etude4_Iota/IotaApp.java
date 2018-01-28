af12864c05f5f64de2645cb5a8011c369d91f1ea
Alexis Barltrop <alexisbarltrop@gmail.com>
2018-01-29 11:20:34 +1300

    updated testing class

--- Etude4_Iota/IotaApp.java
+++ Etude4_Iota/IotaApp.java
@@ -8,12 +8,14 @@ public class IotaApp{
         int p1wins = 0;
         int p2wins = 0;
         Manager m = new Manager();
+        // Put in your players here
         Player p1 = new MaxLots(m);
         Player p2 = new MaxFirst(m);
+        //Change number of tests here
+        int numberOfRounds = 1000;
         m.addPlayers(p1,p2);
-        for(int i = 0; i<1000; i++){
+        for(int i = 0; i<numberOfRounds; i++){
             
-         
             m.setup();
             m.play();
             if (m.getRawScore(p1)> m.getRawScore(p2)){
@@ -22,8 +24,8 @@ public class IotaApp{
                 p2wins++;
             }
         }
-        System.out.println("Player1 wins : " + p1wins);
-        System.out.println("Player2 wins : " + p2wins);
+        System.out.println(p1.getName()  + "  wins : " + p1wins);
+        System.out.println(p2.getName() + "  wins: " + p2wins);
     }
 
 }
