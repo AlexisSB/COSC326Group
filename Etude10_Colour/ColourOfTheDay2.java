716e33c978f85513379659f64a67d0d1a84fd1a8
Anthony Dickson <eight0153@gmail.com>
2018-02-08 16:46:43 +1300

    Memoize colour of the day and primes. Fix factor day counting.

--- Etude10_Colour/ColourOfTheDay2.java
+++ Etude10_Colour/ColourOfTheDay2.java
@@ -6,82 +6,95 @@ import java.util.*;
 public class ColourOfTheDay2 {
     static enum Colour { NONE, RED, GREEN, BLUE, GOLD }
     static final int daysInYear = 350;
+    static Map<Long, Colour> colourOfDay = new HashMap<>(); 
+    static Set<Long> primes = new HashSet<>();
+    static { primes.add(2L); } 
 
     static public boolean isPrime(long n) {
+        if (primes.contains(n)) return true;
         if (n % 2 == 0) return false;
 
-        for (long i = 3; i * i <= n;i += 2) {
+        for (long i = 3; i * i <= n; i += 2) {
             if(n % i == 0) return false;
         }
 
+        primes.add(n);
         return true;
     }
 
     static Colour colour(long day) {
-        if (day == 1) return Colour.NONE;
-        if (day == 7) return Colour.GOLD;
+        Colour c = Colour.NONE;
 
-        if (isPrime(day)) {
+        if (colourOfDay.containsKey(day)) return colourOfDay.get(day);
+        
+        if (day == 1) c = Colour.NONE;
+        else if (day == 7) c = Colour.GOLD;
+        else if (isPrime(day)) {
             long dayOfWeek = day % 7;
-            if (dayOfWeek == 1 || dayOfWeek == 4) return Colour.RED;
-            if (dayOfWeek == 2 || dayOfWeek == 5) return Colour.GREEN;
-            if (dayOfWeek == 3 || dayOfWeek == 6) return Colour.BLUE;
-            return Colour.NONE; 
+            if (dayOfWeek == 1 || dayOfWeek == 4) c = Colour.RED;
+            else if (dayOfWeek == 2 || dayOfWeek == 5) c = Colour.GREEN;
+            else if (dayOfWeek == 3 || dayOfWeek == 6) c = Colour.BLUE;
+            // return Colour.NONE; 
+        } else {
+            Map<Colour, Integer> counts = getCounts(day);
+            int red = counts.get(Colour.RED);
+            int green = counts.get(Colour.GREEN);
+            int blue = counts.get(Colour.BLUE);
+    
+            // If all the same
+            if (red == green && green == blue) {
+                c = Colour.GOLD;
+            }   
+            // If two the same
+            else if (blue == green && blue != red) c = Colour.RED;
+            else if (red == green && red != blue) c = Colour.BLUE;
+            else if (red == blue && red != green) c = Colour.GREEN;
+            // If all diffrent
+            else if (red > green) {
+                if (red > blue) c = Colour.RED;
+                else c = Colour.BLUE;
+            } else {
+                c = Colour.GREEN;
+            }
         }
 
-        Map<Colour, Integer> counts = getCounts(day);
-        int red = counts.get(Colour.RED);
-        int green = counts.get(Colour.GREEN);
-        int blue = counts.get(Colour.BLUE);
-
-        // If all the same
-        if (red == green && green == blue) {
-            return Colour.GOLD;
-        } 
-        
-        // If two the same
-        if (blue == green && blue != red) return Colour.RED;
-        if (red == green && red != blue) return Colour.BLUE;
-        if (red == blue && red != green) return Colour.GREEN;
-
-        // If all diffrent
-        if (red > green) {
-            if (red > blue) return Colour.RED;
-            else return Colour.BLUE;
-        } 
-
-        return Colour.GREEN;
+        colourOfDay.put(day, c);
+        return c;
     }
 
 	private static Map<Colour, Integer> getCounts(long n) {
         Map<Colour, Integer> counts = new HashMap<>();
-        
+
 		for (Colour c : Colour.values()) {
             counts.put(c, 0);
-        }        
-        
-        while (n % 2 == 0) {
-            counts.put(Colour.GREEN, counts.get(Colour.GREEN) + 1);
-            n /= 2;
-        }
-        
-        for (long i = 3; i <= Math.sqrt(n); i += 2) {
-            while (n % i == 0) {
-                counts.put(colour(i), counts.get(colour(i)) + 1);
-                n /= i;
+        }     
+
+        long upper = (long) Math.sqrt(n);
+
+        for (long i = 2; i <= upper; i++) {
+            if (n % i == 0) {
+                Colour c = colour(n - i);
+                counts.put(c, counts.get(c) + 1);
+
+                c = colour(n - (n / i));
+                counts.put(c, counts.get(c) + 1);
             }
         }
-        
-        if (n > 2)  counts.put(colour(n), counts.get(colour(n)) + 1);
 
         return counts;
 	}
 
     public static void main(String[] args) {
-        question1(Long.MAX_VALUE);
+        question1(10000000);
         question2();
         question3();
     }
+
+	private static void printSample() {
+		for (long n = 2; n <= 1000; n++) {
+            System.out.println(n + " " + colour(n));
+        }
+	}
 
 	private static void question3() {
 		long numHolidays = 0;
@@ -115,10 +128,14 @@ public class ColourOfTheDay2 {
             d2 = d3;
             d3 = colour(i);
 
+            if (i % 10000 == 0) System.out.print("\rCURRENT: " + i + " " + d3);
+
             if (d1 == Colour.GOLD && d1 == d2 && d2 == d3) {
-                System.out.println("THREE DAY HOLIDAY ON DAY " + i);
-                break;
+                System.out.println("\nTHREE DAY HOLIDAY ON DAY " + i);
+                return;
             }
         }
+
+        System.out.println("\nNO THREE HOLIDAY IN FIRST " + n + " YEARS");
 	}    
 }
