249c7319eb2fa66e7d388255d2b5509704691a3d
Anthony Dickson <eight0153@gmail.com>
2018-02-09 07:47:54 +1300

    Start writing solver.

---
+++ Etude8_Pentominoes/src/pentominoes/Board.java
@@ -0,0 +1,153 @@
+package pentominoes;
+
+import java.util.*;
+
+/**
+ * Represents a board for a pentomino puzzle.
+ * 
+ * @author Anthony Dickson.
+ */
+public class Board {
+    static final int INVALID = -2;
+    static final int EMPTY = -1;
+
+    int[][] values;
+
+    public Board(List<String> puzzle) {
+        this(puzzle.toArray(new String[0]));
+    }
+    
+    public Board(String... puzzle) {
+        values = new int[puzzle.length][puzzle[0].length()];
+        
+        for (int i = 0; i < puzzle.length; i++) {
+            for (int j = 0; j < puzzle[i].length(); j++) {
+                values[i][j] = (puzzle[i].charAt(j) == '*') ? EMPTY : INVALID;
+            }
+        }
+    }
+    
+    /** Copy constructor. */
+    public Board(Board other) {
+        values = Arrays.stream(other.values).map(int[]::clone).toArray(int[][]::new);
+    }
+    
+    /**
+     * Get the value of the board at position <code>row</code>, <code>col</code>.
+     * 
+     * @param row The row index of the value to get.
+     * @param col The column index of the value to get.
+     * @return The value at <code>row</code>, <code>col</code>. Returns invalid
+     * if the value of the cell was <code>INVALID</code> or indicies were out 
+     * of bounds.
+     * 
+     * @author Anthony Dickson.
+     */
+    public int get(int row, int col) {
+        try {
+            return values[row][col];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // System.err.println("Indicies " + row + " " + col + " out of bounds.");
+        }
+        
+        return INVALID;
+    }
+
+    /**
+     * Sets the value of the board at position <code>row</code>, 
+     * <code>col</code> to the value of <code>type</code>.
+     * 
+     * @param row The row index of the value to set.
+     * @param col The column index of the value to set.
+     * @param type The value, or type of pentomino, to set the cell to.
+     * @return <code>true</code> if the value was successfully set. 
+     * Returns <code>false</code> if indicies are out of bounds, the cell
+     * is not empty, or the cell is invalid.
+     * 
+     * @author Anthony Dickson.
+     */
+    public boolean set(int row, int col, Type type) {
+        try {
+            if (values[row][col] == EMPTY) {
+                values[row][col] = type.ordinal();
+                return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            // System.err.println("Indicies " + row + " " + col + " out of bounds.");
+        }
+
+        return false; 
+    }
+
+    /**
+     * Places the pentomino <code>p</code> on the board at position 
+     * <code>row</code>, <code>col</code>.
+     * 
+     * @param row The row index of where to place the top left corner of 
+     * the pentomino <code>p</code>.
+     * @param col The column index of where to place the top left corner of 
+     * the pentomino <code>p</code>.
+     * @param p The pentomino to place.
+     * @return <code>true</code> if the pentomino was successfully placed. 
+     * Returns <code>false</code> otherwise.
+     * 
+     * @author Anthony Dickson.
+     */
+    public boolean place(int row, int col, Pentomino p) {
+        for (Coordinate c : p.coordinates) {
+            if (!set(row + c.y, col + c.x, p.type)) {
+                remove(row, col, p);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void remove(int row, int col, Pentomino p) {
+        for (Coordinate c : p.coordinates) {
+            try {
+                if (values[row + c.y][col + c.x] == p.type.ordinal()) {
+                    values[row + c.y][col + c.x] = EMPTY;
+                }
+            } catch (ArrayIndexOutOfBoundsException e) {
+                // System.err.println("Indicies " + row + " " + col + " out of bounds.");
+            }
+        }
+    }
+
+    public void remove(Pentomino p) {
+        for (int[] row : values) {
+            for (int col = 0; col < row.length; col++) {
+                if (row[col] == p.type.ordinal()) {
+                    row[col] = EMPTY;
+                }
+            }
+        }
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+
+        for (int[] row : values) {
+            for (int col : row) {
+                if (col == INVALID) {
+                    str.append(" ");
+                } else if (col == EMPTY) {
+                    str.append("_");
+                } else if (col < -2) {
+                    str.append(col);
+                } else {
+                    str.append(Type.values()[col]);
+                }
+
+                str.append(" ");   
+            }
+
+            str.append("\n");
+        }
+
+        return str.toString();
+    }
+}
--- Etude8_Pentominoes/src/pentominoes/Solver.java
+++ Etude8_Pentominoes/src/pentominoes/Solver.java
@@ -1,4 +1,156 @@
 package pentominoes;
 
+import java.util.*;
+
 public class Solver {
+    static ArrayDeque<State> states = new ArrayDeque<>();
+    static StringBuilder ticker = new StringBuilder();
+    static long updated = System.currentTimeMillis();
+
+    public static void solve(Board puzzle) {
+        System.out.println(puzzle);
+
+        states.clear();
+        states.add(new State(puzzle, new HashSet<>()));
+        
+        if (!search()) System.out.println("Impossible.");
+        
+        for (State s : states) {
+            System.err.println(s.board);
+        }
+
+        System.out.println(states.peekLast().board);
+    }
+
+    private static boolean search() {
+        State state = states.peekLast();
+        Board board = state.board;
+        Set<Type> placed = state.placed;
+
+        if (System.currentTimeMillis() - updated > 1000) {
+            updated = System.currentTimeMillis();
+            if (ticker.length() > 2) ticker = new StringBuilder();
+            ticker.append(".");
+            System.out.format("\r%-3s", ticker.toString());
+            System.err.println(board);
+        }
+        
+        for (Type t : Type.values()) {
+            if (placed.contains(t)) continue;
+
+            for (Pentomino p : Pentomino.PENTOMINOES.get(t)) {
+                if (place(p, board)) {
+                    placed.add(p.type);
+                    states.add(new State(board, placed));
+                    System.err.println(board);
+                    
+                    if (search()) {
+                        return true;
+                    } else {
+                        states.pollLast(); 
+                        placed.remove(p.type);   
+                        board.remove(p);
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    private static boolean place(Pentomino p, Board board) {
+        for (int row = 0; row < board.values.length; row++) {
+            for (int col = 0; col < board.values[row].length; col++) {
+                if (board.place(row, col, p)) {
+                    if (hasHole(board)) {
+                        board.remove(row, col, p);
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static boolean hasHole(Board board) {
+        int visitedVal = -3;
+        int numEmpty = 0;
+        int total = 0;
+
+        for (int row = 0; row < board.values.length; row++) {
+            for (int col = 0; col < board.values[row].length; col++) {
+                int count = countEmpty(board, row, col, visitedVal);
+                if (count != 0) {
+                    visitedVal--;
+                    total += count;
+                } else {
+                    continue;
+                } 
+                if (count % 5 != 0) {
+                    System.err.print(board);
+                    System.err.println(total);
+                    cleanup(board);
+                    return true;
+                }
+            }
+        }
+
+        cleanup(board);
+        return false;
+    }
+
+    private static int countEmpty(Board board, int row, int col, int visitedVal) {
+        int count = 0;
+
+        if (board.get(row, col) == Board.EMPTY) {
+            board.values[row][col] = visitedVal;
+            count++;
+
+            count += countEmpty(board, row, col - 1, visitedVal);
+            count += countEmpty(board, row, col + 1, visitedVal);
+            count += countEmpty(board, row - 1, col, visitedVal);
+            count += countEmpty(board, row + 1, col, visitedVal);
+        }
+
+        return count;
+    }
+
+    private static void cleanup(Board board) {
+        for (int row = 0; row < board.values.length; row++) {
+            for (int col = 0; col < board.values[row].length; col++) {
+                if (board.get(row, col) < -2) {
+                    board.values[row][col] = Board.EMPTY;
+                }
+            }
+        }
+    }
+    
+    private static class State {
+        final Board board;
+        final Set<Type> placed;
+
+        public State(Board board, Set<Type> placed) {
+            this.board = new Board(board);
+            this.placed = new HashSet<>(placed);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<String> puzzle = new ArrayList<>();
+        
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            
+            if (!line.equals("")) puzzle.add(line);
+
+            if (line.equals("") || !in.hasNextLine()) {
+                Solver.solve(new Board(puzzle));
+            }
+        }
+
+        in.close();
+    }
 }
