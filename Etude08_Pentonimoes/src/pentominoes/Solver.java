2b1d9ea9cade47c6973205ca45f0eb9988a86dcc
Anthony Dickson <eight0153@gmail.com>
2018-02-08 13:38:07 +1300

    Finish upto pentomino R

--- Etude08_Pentonimoes/src/pentominoes/Solver.java
+++ Etude08_Pentonimoes/src/pentominoes/Solver.java
@@ -57,28 +57,28 @@ public class Solver {
                                         new Coordinate(1, 1),
                                         new Coordinate(2, 1)));
 
-        // P, Identity, Flipped
+        // P, Flipped, Identity
         P.add(new Pentomino(Encoding.P, new Coordinate(0, 0),
                                         new Coordinate(1, 0),
                                         new Coordinate(0, 1),
                                         new Coordinate(1, 1),
                                         new Coordinate(1, 2)));
 
-        // P, Rotated 90 degrees, Flipped
+        // P, Flipped, Rotated 90 degrees
         P.add(new Pentomino(Encoding.P, new Coordinate(1, 0),
                                         new Coordinate(2, 0),
                                         new Coordinate(0, 1),
                                         new Coordinate(1, 1),
                                         new Coordinate(2, 1)));
                                         
-        // P, Rotated 180 degrees, Flipped
+        // P, Flipped, Rotated 180 degrees
         P.add(new Pentomino(Encoding.P, new Coordinate(0, 0),
                                         new Coordinate(0, 1),
                                         new Coordinate(1, 1),
                                         new Coordinate(0, 2),
                                         new Coordinate(1, 2)));
         
-        // P, Rotated 270 degrees, Flipped
+        // P, Flipped, Rotated 270 degrees
         P.add(new Pentomino(Encoding.P, new Coordinate(0, 0),
                                         new Coordinate(1, 0),
                                         new Coordinate(2, 0),
@@ -118,28 +118,28 @@ public class Solver {
                                         new Coordinate(3, 0),
                                         new Coordinate(0, 1)));
         
-        // Q, Identity, Flipped
+        // Q, Flipped, Identity
         Q.add(new Pentomino(Encoding.Q, new Coordinate(0, 0),
                                         new Coordinate(1, 0),
                                         new Coordinate(0, 1),
                                         new Coordinate(0, 2),
                                         new Coordinate(0, 3)));                                
         
-        // Q, Rotated 90 degrees, Flipped
+        // Q, Flipped, Rotated 90 degrees
         Q.add(new Pentomino(Encoding.Q, new Coordinate(0, 0),
                                         new Coordinate(1, 0),
                                         new Coordinate(2, 0),
                                         new Coordinate(3, 0),
                                         new Coordinate(3, 1)));                                
         
-        // Q, Rotated 180 degrees, Flipped
+        // Q, Flipped, Rotated 180 degrees
         Q.add(new Pentomino(Encoding.Q, new Coordinate(1, 0),
                                         new Coordinate(1, 1),
                                         new Coordinate(1, 2),
                                         new Coordinate(0, 3),
                                         new Coordinate(1, 3)));                                
         
-        // Q, Rotated 270 degrees, Flipped
+        // Q, Flipped, Rotated 270 degrees
         Q.add(new Pentomino(Encoding.Q, new Coordinate(0, 0),
                                         new Coordinate(0, 1),
                                         new Coordinate(1, 1),
@@ -147,18 +147,71 @@ public class Solver {
                                         new Coordinate(3, 1)));                                
 
         PENTOMINOES.put(Encoding.Q, Q);
+
+        // Pentomino R
+        List<Pentomino> R = new ArrayList<>();
+
+        // R, Identity
+        R.add(new Pentomino(Encoding.R, new Coordinate(1, 0),
+                                        new Coordinate(2, 0),
+                                        new Coordinate(0, 1),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(1, 2)));
+
+        // R, Rotated 90 degrees
+        R.add(new Pentomino(Encoding.R, new Coordinate(1, 0),
+                                        new Coordinate(0, 1),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(2, 1),
+                                        new Coordinate(2, 2)));
+
+        // R, Rotated 180 degrees
+        R.add(new Pentomino(Encoding.R, new Coordinate(1, 0),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(2, 1),
+                                        new Coordinate(0, 2),
+                                        new Coordinate(1, 2)));
+
+        // R, Rotated 270 degrees
+        R.add(new Pentomino(Encoding.R, new Coordinate(0, 0),
+                                        new Coordinate(0, 1),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(2, 1),
+                                        new Coordinate(1, 2)));
+
+        // R, Flipped, Identity
+        R.add(new Pentomino(Encoding.R, new Coordinate(0, 0),
+                                        new Coordinate(1, 0),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(2, 1),
+                                        new Coordinate(1, 2)));
+
+        // R, Flipped, Rotated 90 degrees
+        R.add(new Pentomino(Encoding.R, new Coordinate(0, 0),
+                                        new Coordinate(1, 0),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(2, 1),
+                                        new Coordinate(1, 2)));
+
+        // R, Flipped, Rotated 180 degrees
+        R.add(new Pentomino(Encoding.R, new Coordinate(1, 0),
+                                        new Coordinate(0, 1),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(1, 2),
+                                        new Coordinate(2, 2)));
+
+        // R, Flipped, Rotated 270 degrees
+        R.add(new Pentomino(Encoding.R, new Coordinate(0, 0),
+                                        new Coordinate(0, 1),
+                                        new Coordinate(1, 1),
+                                        new Coordinate(2, 1),
+                                        new Coordinate(1, 2)));
+
+        PENTOMINOES.put(Encoding.R, R);
     }
                                     
     public static void main(String[] args) {
-        for (Pentomino p : PENTOMINOES.get(Encoding.O)) {
-            System.out.println(p);
-        }
-
-        for (Pentomino p : PENTOMINOES.get(Encoding.P)) {
-            System.out.println(p);
-        }
-
-        for (Pentomino p : PENTOMINOES.get(Encoding.Q)) {
+        for (Pentomino p : PENTOMINOES.get(Encoding.R)) {
             System.out.println(p);
         }
     }
