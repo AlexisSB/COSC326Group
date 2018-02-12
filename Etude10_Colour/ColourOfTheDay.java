1d4f5b2b0768c3ce03e750ba1ffd0967e0108b9d
Anthony Dickson <eight0153@gmail.com>
2018-02-12 14:05:07 +1300

    Merge missed merge conflicts.

--- Etude10_Colour/ColourOfTheDay.java
+++ Etude10_Colour/ColourOfTheDay.java
@@ -54,11 +54,7 @@ public class ColourOfTheDay {
                 if (red > blue) c = Colour.RED;
                 else c = Colour.BLUE;
             } else {
-<<<<<<< HEAD
                 if (green > blue) c = Colour.GREEN;
-=======
-                if (green > blue) c  = Colour.GREEN;
->>>>>>> 725e76bedd78bd43401d2e7d5c59882bc40fad0b
                 else c = Colour.BLUE;
             }
         }
